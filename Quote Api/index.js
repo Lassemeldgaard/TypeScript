4846669b496c2aa5bbc212de9a627af2ba77beb2
Lasse Rüss Meldgaard <lass942b@student.eadania.dk>
2017-09-19 13:45:53 +0200

    html

--- Quote Api/index.js
+++ Quote Api/index.js
@@ -8,45 +8,72 @@ app.use(BodyParser.urlencoded({ extended: true }));
 var quotes = [{
         title: "Awesome quote",
         description: 'This is an awesome quote made by lasse',
-        rating: 0
+        rating: 2,
+        date: new Date("Wed Sep 20 2017")
     },
     {
         title: "Awesome quote2",
         description: 'This is an awesome quote 2 made by lasse',
-        rating: 1
+        rating: 3,
+        date: new Date("2017-09-18")
+    },
+    {
+        title: "Awesome quote3",
+        description: 'This is an awesome quote 2 made by lasse',
+        rating: 1,
+        date: new Date("2017-09-19")
     }];
-app.post("/api", function (req, resp) {
+app.post("/quotes/:id/downvote", function (req, resp) {
     var result;
-    var func = req.body.function;
-    if (func === "add") {
-        var title = req.body.title;
-        var description = req.body.description;
-        var rating = req.body.rating;
-        quotes.push({ title: title, description: description, rating: rating });
-        result = "added";
-    }
-    else if (func === "list") {
-        result = quotes;
-    }
-    else if (func === "show") {
-        var id = req.body.id;
-        result = quotes[id];
-    }
-    else if (func === "delete") {
-        var id = req.body.id;
-        quotes.splice(id, 1);
-        result = "Deleted";
-    }
-    else if (func === "upvote") {
-        var id = req.body.id;
-        quotes[id].rating++;
-        result = "Upvoted";
-    }
-    else if (func === "downvote") {
-        var id = req.body.id;
-        quotes[id].rating--;
-        result = "Downvoted";
-    }
+    var id = req.params.id;
+    quotes[id].rating--;
+    result = "Downvoted";
+    resp.json(result);
+});
+app.post("/quotes/:id/upvote", function (req, resp) {
+    var result;
+    var id = req.params.id;
+    quotes[id].rating++;
+    result = "Upvoted";
+    resp.json(result);
+});
+app.post("/quotes/:id/delete", function (req, resp) {
+    var result;
+    var id = req.params.id;
+    quotes.splice(id, 1);
+    result = "Deleted";
+    resp.json(result);
+});
+app.post("/quotes/add", function (req, resp) {
+    var title = req.body.title;
+    var description = req.body.description;
+    var rating = 0;
+    var date = new Date();
+    quotes.push({ title: title, description: description, rating: rating, date: date });
+    var result = "added";
+    resp.json(result);
+});
+app.post("/quotes/show/:id", function (req, resp) {
+    var result;
+    var id = req.params.id;
+    result = quotes[id];
+    resp.json(result);
+});
+app.post("/quotes", function (req, resp) {
+    var result;
+    result = quotes;
+    resp.json(result);
+});
+app.post("/quotes/highestrated", function (req, resp) {
+    var result;
+    quotes.sort(function (a, b) { return b.rating > a.rating ? 1 : -1; });
+    result = quotes;
+    resp.json(result);
+});
+app.post("/quotes/newest", function (req, resp) {
+    var result;
+    quotes.sort(function (a, b) { return a.date - b.date; });
+    result = quotes;
     resp.json(result);
 });
 console.log("server listing on port 3000");
--- Quote Api/index.ts
+++ Quote Api/index.ts
@@ -3,55 +3,86 @@ let app = Express();
 
 import * as BodyParser from 'body-parser'
 app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({extended: true}))
+app.use(BodyParser.urlencoded({ extended: true }))
 
-let quotes = [{
+let quotes: any[] = [{
     title: "Awesome quote",
     description: 'This is an awesome quote made by lasse',
-    rating: 0
+    rating: 2,
+    date: new Date("Wed Sep 20 2017")
 },
 {
     title: "Awesome quote2",
     description: 'This is an awesome quote 2 made by lasse',
-    rating: 1
+    rating: 3,
+    date: new Date("2017-09-18")
+},
+{
+    title: "Awesome quote3",
+    description: 'This is an awesome quote 2 made by lasse',
+    rating: 1,
+    date: new Date("2017-09-19")
 }];
 
-app.post("/api", (req, resp) => {
+app.post("/quotes/:id/downvote", (req, resp) => {
     let result;
-    let func = req.body.function;
-    
+    let id = req.params.id;
+    quotes[id].rating--;
+    result = "Downvoted";
 
-    if(func === "add"){
-        let title = req.body.title;
-        let description = req.body.description;
-        let rating = req.body.rating
-        quotes.push({title, description, rating});
-        result = "added";
-    }
-    else if(func === "list"){
-        result = quotes;
-    }
-    else if(func === "show"){
-        let id = req.body.id;
-        result = quotes[id];
-    }
-    else if (func === "delete"){
-        let id = req.body.id;
-        quotes.splice(id, 1);
-        result = "Deleted";
-    }
-    else if(func === "upvote"){
-        let id = req.body.id;
-        quotes[id].rating++;
-        result ="Upvoted";
-    }
-    else if(func === "downvote"){
-        let id = req.body.id;
-        quotes[id].rating--;
-        result ="Downvoted";
-    }
     resp.json(result);
 });
+app.post("/quotes/:id/upvote", (req, resp) => {
+    let result;
+    let id = req.params.id;
+    quotes[id].rating++;
+    result = "Upvoted";
+
+    resp.json(result);
+});
+app.post("/quotes/:id/delete", (req, resp) => {
+    let result;
+    let id = req.params.id;
+    quotes.splice(id, 1);
+    result = "Deleted";
+
+    resp.json(result);
+});
+app.post("/quotes/add", (req, resp) => {
+    let title = req.body.title;
+    let description = req.body.description;
+    let rating = 0;
+    let date = new Date();
+    quotes.push({ title, description, rating, date });
+    let result = "added";
+    resp.json(result);
+});
+app.post("/quotes/show/:id", (req, resp) => {
+    let result;
+    let id = req.params.id;
+    result = quotes[id];
+
+    resp.json(result);
+});
+app.post("/quotes", (req, resp) => {
+    let result;
+    result = quotes;
+    resp.json(result);
+});
+app.post("/quotes/highestrated", (req, resp) => {
+    let result;
+    quotes.sort((a,b) => b.rating > a.rating ? 1 : -1)
+    result = quotes;
+    resp.json(result);
+});
+app.post("/quotes/newest", (req, resp) => {
+    let result;
+    quotes.sort((a,b) => a.date - b.date)
+    result = quotes;
+    resp.json(result);
+});
+
+
 
 console.log("server listing on port 3000");
 app.listen(3000);
